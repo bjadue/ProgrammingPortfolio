d1dd2cc5f674051f3e8b2c597c5d99581e7b7998
Brandon <103289054+bjadue@users.noreply.github.com>
2025-07-07 11:42:13 -0400

    Update RemoteRename.ps1
    its 15 not 16 :(

--- RemoteRename/RemoteRename.ps1
+++ RemoteRename/RemoteRename.ps1
@@ -1,21 +1,21 @@
 #written by Brandon Jadue, in part from
 #https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-7.5
 
-$lessThan16 = $true
-$16response = "y"
-#default variables regarding the target name being above 16 characters
+$lessThan15 = $true
+$15response = "y"
+#default variables regarding the target name being above 15 characters
 write-output "Please note that target PC must already be added to the domain.`n"
 $target = Read-Host "Please enter host name of target PC"
 $nombre = Read-Host "Please enter new host name of target PC"
 
-if ($nombre.length -gt 16){
-	$lessThan16 = $false
-	Write-Host "WARNING: `nNew host name of target PC is longer than 16 characters. This can cause issues with Azure AD, SQL, etc.." -ForegroundColor Red
-	$16response = Read-Host "Would you like to continue with the process? (CAN CAUSE ISSUES) [y/n]" -ForegroundColor Yellow
+if ($nombre.length -gt 15){
+	$lessThan15 = $false
+	Write-Host "WARNING: `nNew host name of target PC is longer than 15 characters. This can cause issues with Azure AD, SQL, etc.." -ForegroundColor Red
+	$15response = Read-Host "Would you like to continue with the process? (CAN CAUSE ISSUES) [y/n]" -ForegroundColor Yellow
 }
 
-if ($16response -eq "n"){
-	$nombre = Read-Host "Please enter new host name of target PC [16>= characters]"
+if ($15response -eq "n"){
+	$nombre = Read-Host "Please enter new host name of target PC [15>= characters]"
 }
 
 $creds = get-credential
