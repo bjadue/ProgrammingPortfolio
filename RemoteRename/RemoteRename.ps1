5c74af4384e009912b2406e70fed035b196d6fa8
Brandon <103289054+bjadue@users.noreply.github.com>
2025-08-06 12:23:16 -0400

    Update RemoteRename.ps1
    Turned some recurring parts into functions.

--- RemoteRename/RemoteRename.ps1
+++ RemoteRename/RemoteRename.ps1
@@ -4,20 +4,34 @@
 $lessThan15 = $true
 $15response = "y"
 #default variables regarding the target name being above 15 characters
+function Get-Length {
+	if ($nombre.length -gt 15){
+		$lessThan15 = $false
+		Write-Host "`nWARNING: `nNew host name of target PC is longer than 15 characters. This can cause issues with Azure AD, SQL, etc.." -ForegroundColor red -BackgroundColor black
+		$15response = Read-Host "Type [n] to cancel this process and correct the name, otherwise press enter" 
+	}
+	if ($nombre.length -lt 15) {
+		$lessThan15 = $true
+		Write-Host "`nNew name is good. Continuing..." -ForegroundColor Green 
+		return 
+	}
+	if ($15response -eq "n"){
+		Redo-Length
+	}else{
+		Write-Host "`nUser opted to continue." -ForegroundColor Yellow 
+		return 
+	}
+}
+function Redo-Length {
+	$nombre = Read-Host "Please enter new host name of target PC [15>= characters]"
+	Write-Host "`nVerifying..." -ForegroundColor Magenta
+	Get-Length
+}
+
 write-output "Please note that target PC must already be added to the domain.`n"
 $target = Read-Host "Please enter host name of target PC"
 $nombre = Read-Host "Please enter new host name of target PC"
-
-if ($nombre.length -gt 15){
-	$lessThan15 = $false
-	Write-Host "WARNING: `nNew host name of target PC is longer than 15 characters. This can cause issues with Azure AD, SQL, etc.." -ForegroundColor Red
-	$15response = Read-Host "Would you like to continue with the process? (CAN CAUSE ISSUES) [y/n]" -ForegroundColor Yellow
-}
-
-if ($15response -eq "n"){
-	$nombre = Read-Host "Please enter new host name of target PC [15>= characters]"
-}
-
+Get-Length
 $creds = get-credential
 $renameParams = @{
     ComputerName = "$target"
@@ -29,4 +43,4 @@ $renameParams = @{
 }
 #run cmdlet with the pre-defined parameters
 rename-computer @renameParams
-exit 0
+
