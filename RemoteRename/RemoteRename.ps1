18bf2c373702d576e2f5638e3def249692873a07
Brandon <103289054+bjadue@users.noreply.github.com>
2025-08-07 12:59:47 -0400

    Update RemoteRename.ps1
    fallen into if-statement hell get me out of here

--- RemoteRename/RemoteRename.ps1
+++ RemoteRename/RemoteRename.ps1
@@ -1,33 +1,34 @@
 #written by Brandon Jadue, in part from
 #https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-7.5
-
 $lessThan15 = $true
 $15response = "y"
 #default variables regarding the target name being above 15 characters
 function Get-Length {
+	Write-Host "`nVerifying new name $nombre..." -ForegroundColor Magenta
 	if ($nombre.length -gt 15){
+		Write-Host "$nombre did not pass check." -ForegroundColor Red
 		$lessThan15 = $false
 		Write-Host "`nWARNING: `nNew host name of target PC is longer than 15 characters. This can cause issues with Azure AD, SQL, etc.." -ForegroundColor red -BackgroundColor black
-		$15response = Read-Host "Type [n] to cancel this process and correct the name, otherwise press enter" 
-	}
-	if ($nombre.length -lt 15) {
-		$lessThan15 = $true
-		Write-Host "`nNew name is good. Continuing..." -ForegroundColor Green 
-		return 
-	}
-	if ($15response -eq "n"){
-		Redo-Length
+		$15response = Read-Host "Type [n] to cancel this process and correct the name, otherwise press enter`n" 
 	}else{
-		Write-Host "`nUser opted to continue." -ForegroundColor Yellow 
-		return 
+		$lessThan15 = $true  
+	}
+
+	if ($15response -eq "n" -and $lessThan15 -eq $false){
+		Redo-Length
+	}elseif($15response -ne "n" -and $lessThan15 -eq $false){
+		#user force bypass
+		Write-Host "User opted to continue." -ForegroundColor Yellow 
+		return
+	}elseif($lessThan15 -eq $true){
+		Write-Host "Name is good. Continuing..." -ForegroundColor Green
+		return
 	}
 }
 function Redo-Length {
 	$nombre = Read-Host "Please enter new host name of target PC [15>= characters]"
-	Write-Host "`nVerifying..." -ForegroundColor Magenta
 	Get-Length
 }
-
 write-output "Please note that target PC must already be added to the domain.`n"
 $target = Read-Host "Please enter host name of target PC"
 $nombre = Read-Host "Please enter new host name of target PC"
