568c2ab1fa9b620a7d6a0e9e89b1211b744a23f2
Brandon <103289054+bjadue@users.noreply.github.com>
2025-08-15 12:01:01 -0400

    Update RemoteRename.ps1
    why did it go in twice what

--- RemoteRename/RemoteRename.ps1
+++ RemoteRename/RemoteRename.ps1
@@ -49,57 +49,6 @@ $renameParams = @{
 }
 #run cmdlet with the pre-defined parameters
 rename-computer @renameParams
-#written by Brandon Jadue, in part from
-#https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-7.5
-$lessThan15 = $true
-$15response = "y"
-#default variables regarding the target name being above 15 characters
-function Get-Length {
-	Write-Host "`nVerifying new name $nombre..." -ForegroundColor Magenta
-	if ($nombre.length -gt 15){
-		Write-Host "$nombre did not pass check." -ForegroundColor Red
-		$lessThan15 = $false
-		Write-Host "`nWARNING: `nNew host name of target PC is longer than 15 characters. This can cause issues with Azure AD, SQL, etc.." -ForegroundColor red -BackgroundColor black
-		$15response = Read-Host "Type [n] to cancel this process and correct the name, otherwise press enter`n" 
-	}else{
-		$lessThan15 = $true  
-	}
-	if ($15response -eq "n" -and $lessThan15 -eq $false){
-		Redo-Length
-	}elseif($15response -ne "n" -and $lessThan15 -eq $false){
-		#user force bypass
-		Write-Host "User opted to continue." -ForegroundColor Yellow 
-		return
-	}elseif($lessThan15 -eq $true){
-		Write-Host "Name is good. Continuing..." -ForegroundColor Green
-		return
-	}else{
- 		Write-Host "`nFatal error. Aborting." -ForegroundColor red -BackgroundColor black
- 		exit 1
-  	}
-}
-function Redo-Length {
-	$nombre = ""
- 	#reset variable, to prevent any issues in case it does not change
-	$nombre = Read-Host "Please enter new host name of target PC [15>= characters]"
-	Get-Length($nombre)
-}
-write-output "Please note that target PC must already be added to the domain.`n"
-$target = Read-Host "Please enter host name of target PC"
-$nombre = Read-Host "Please enter new host name of target PC"
-#entering potential loop
-Get-Length
-$creds = get-credential
-$renameParams = @{
-    ComputerName = "$target"
-    NewName = "$nombre"
-    DomainCredential = $creds
-    Force = $true
-    PassThru = $true
-    Restart = $true
-}
-#run cmdlet with the pre-defined parameters
-rename-computer @renameParams
 # SIG # Begin signature block
 # MIIFhgYJKoZIhvcNAQcCoIIFdzCCBXMCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
@@ -132,3 +81,4 @@ rename-computer @renameParams
 # g33ru7Wuhe40wep2e0Q/ivIJUQQbMRWIN630gkU/2gzAe8Fkzl18v7DpzXEe4vNH
 # kKoQJOpT/nQA7nrtTWAHhn+9ndPDUXEx+HE=
 # SIG # End signature block
+
